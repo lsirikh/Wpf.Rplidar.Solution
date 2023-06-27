269f472daa847923a3522cfc508b760fff6d2287
이기호 <gh.lee0786@gmail.com>
2023-06-27 23:38:34 +0900

    updated

---
+++ Wpf.Rplidar.Solution/Services/LidarService.cs
@@ -0,0 +1,36 @@
+﻿namespace Wpf.Rplidar.Solution.Services
+{
+    /****************************************************************************
+        Purpose      :                                                           
+        Created By   : GHLee                                                
+        Created On   : 6/27/2023 11:32:21 PM                                                    
+        Department   : SW Team                                                   
+        Company      : Sensorway Co., Ltd.                                       
+        Email        : lsirikh@naver.com                                         
+     ****************************************************************************/
+
+    public class LidarService
+    {
+
+        #region - Ctors -
+        public LidarService()
+        {
+
+        }
+        #endregion
+        #region - Implementation of Interface -
+        #endregion
+        #region - Overrides -
+        #endregion
+        #region - Binding Methods -
+        #endregion
+        #region - Processes -
+        #endregion
+        #region - IHanldes -
+        #endregion
+        #region - Properties -
+        #endregion
+        #region - Attributes -
+        #endregion
+    }
+}
